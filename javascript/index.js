327f3d05166c51715c806e0da76e742cadffa8ad
Guytanthong <tanthongguy292@gmail.com>
2026-03-21 17:08:25 +0700

    randomnum generator

--- javascript/index.js
+++ javascript/index.js
@@ -1,7 +1,37 @@
-let username;
+//counter program
 
-
-document.getElementById("mysubmit").onclick = function(){
-    username = document.getElementById("mytext").value;
-    document.getElementById("myh1").textContent =`Hello ${username}`;
+const decreaseBtn = document.getElementById("decrement");
+const increaseBtn = document.getElementById("increment");
+const resetBtn = document.getElementById("reset");
+const countLabel = document.getElementById("display");
+let count = 0;
+increaseBtn.onclick = function(){
+    count++;
+    countLabel.textContent = count;
 }
+
+decreaseBtn.onclick = function(){
+    count--;
+    countLabel.textContent = count;
+}
+
+resetBtn.onclick = function(){
+    count = 0;
+    countLabel.textContent = count;
+}
+
+//random number generator
+const rollBtn = document.getElementById("myButton");
+const label = document.getElementById("myLabel");
+
+const min = 1;
+const max = 6;
+
+let randomNum;
+
+rollBtn.onclick = function(){
+    randomNum = Math.floor((Math.random() * max)) + min;
+    label.textContent = randomNum;
+}
+
+
