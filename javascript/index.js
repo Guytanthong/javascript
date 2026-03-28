01cb1ae3aa0836d9e601b8b7e60e34642e532b24
Guytanthong <tanthongguy292@gmail.com>
2026-03-28 16:45:21 +0700

    2th

--- javascript/index.js
+++ javascript/index.js
@@ -34,4 +34,59 @@ rollBtn.onclick = function(){
     label.textContent = randomNum;
 }
 
+//Number guessing game
 
+// const minNum = 1;
+// const maxNum = 100;
+// const answer = Math.floor(Math.random() * (maxNum - minNum + 1)) + minNum;
+
+// let attempts = 0;
+// let guess ;
+// let running = true;
+
+// while(running){
+
+//     guess = window.prompt(`Guess a number between ${minNum} - ${maxNum}`);
+//     guess = Number(guess);
+
+//     if(isNaN(guess)){
+//         window.alert("Please enter a valid number");
+//     }else if(guess < minNum || guess >maxNum){
+//         window.alert("Please enter a valid number");
+
+//     }else{
+//         attempts++;
+//         if(guess < answer){
+//             window.alert("Too Low");
+//         }else if(guess > answer){
+//             window.alert("Too High");
+//         }else{
+//             window.alert("Correct");
+//             running = false;
+//         }
+//     }
+
+   
+// }
+
+// temp conve
+const textbox = document.getElementById("textbox");
+const toF = document.getElementById("toF");
+const toC = document.getElementById("toC");
+const res = document.getElementById("res");
+
+let temp;
+
+function convert(){
+    if(toF.checked){
+            temp = Number(textbox.value);
+            temp = temp * 9 / 5 + 32;
+            res.textContent= temp + "F"
+    }else if(toC.checked){
+        temp = Number(textbox.value);
+        temp = (temp -32) *(5/9);
+        res.textContent= temp + "C"
+    }else{
+        res.textContent= "select a unit"
+    }
+}
